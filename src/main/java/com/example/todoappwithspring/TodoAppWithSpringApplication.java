da13226cc32db49a2351d37b9555bd0318866bce
metin-gercek <metin.gercek@edu.bc.fi>
2022-10-20 13:36:07 +0300

    create app

---
+++ src/main/java/com/example/todoappwithspring/TodoAppWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoappwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todoappwithspring/TodoAppWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoappwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
